c1b95990d4ac7fa8b8cf96a62dd1af2713eb2ff8
derekbl17 <derekc1fill@gmail.com>
2025-04-07 18:30:47 +0300

    z

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <h1>COUNTRY API</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-
     </>
   )
 }
